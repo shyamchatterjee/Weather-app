f412d37d73cffb53bf798b4cb6154d93c2c6cd4e
shyamdyuti <chatterjeeshyamdyuti@gmail.com>
2025-03-09 12:44:53 +0530

    Added contast file and create api

--- src/component/resultcomponent.jsx
+++ src/component/resultcomponent.jsx
@@ -1,6 +1,6 @@
 let Result = ()=>{
    return <div className="wether-container">
-    <img src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png" width={200}     height={200}   alt="" />
+    <img src="" width={300}     height={200}   alt="" />
           <p style={{fontSize:"55px"}}>22c</p>
           <p>New York</p>
     </div>
