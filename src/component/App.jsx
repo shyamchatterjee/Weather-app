350c3e9c1ed4fbea5156610dd3a00f3e5d2ea126
shyamdyuti <chatterjeeshyamdyuti@gmail.com>
2025-03-09 08:37:31 +0530

    Added project structure and initial project and similar designing

---
+++ src/component/App.jsx
@@ -0,0 +1,8 @@
+import '../App.css'
+import Search from './Searchcomponent'
+let App = ()=>{
+    return <div className="container">
+              <Search/>
+    </div>
+}
+export default App
---
+++ src/component/Searchcomponent.jsx
@@ -0,0 +1,9 @@
+import { IoIosSearch } from "react-icons/io";
+
+let Search = ()=>{
+    return <div className="serach-container">
+            <IoIosSearch/>
+          <input type="text" name="" id="input-text" placeholder="Search" />
+    </div>
+}
+export default Search
