e40e00cadf237adb035feca62ad364afa8bd9f23
shyamdyuti <chatterjeeshyamdyuti@gmail.com>
2025-03-09 11:21:37 +0530

    npm install json-server

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,8 +1,10 @@
 import '../App.css'
+import Result from './resultcomponent'
 import Search from './Searchcomponent'
 let App = ()=>{
     return <div className="container">
               <Search/>
+              <Result/>
     </div>
 }
 export default App
